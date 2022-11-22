d2c7ca1ad84d2235f92be47b63c01d57a6379cae
MPrathyusha01 <prathyusha4501@gmail.com>
2022-11-22 13:22:20 +0530

    control 22-11

---
+++ controlStatements/src/controlStatements/ifCondition.java
@@ -0,0 +1,13 @@
+package controlStatements;
+
+public class ifCondition {
+
+	public static void main(String[] args) {
+		int a = 12;
+		if (a >10) {
+			System.out.println("A islesser than 10");
+		}
+
+	}
+
+}
