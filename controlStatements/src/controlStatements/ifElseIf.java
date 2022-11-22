bae5c4e441c0bb7642712f61a5cf66df1e4e9ad4
MPrathyusha01 <prathyusha4501@gmail.com>
2022-11-22 13:33:08 +0530

    22-11 control statements

---
+++ controlStatements/src/controlStatements/ifElseIf.java
@@ -0,0 +1,17 @@
+package controlStatements;
+
+public class ifElseIf {
+
+	public static void main(String[] args) {
+		int a = 0;
+		if(a<0) {
+			System.out.println("A is negative");
+		} else if(a>0) {
+			System.out.println("A is negative");
+		} else {
+			System.out.println("A is zero");
+		}
+
+	}
+
+}
